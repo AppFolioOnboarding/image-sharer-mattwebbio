ed380be012f17bda58c3ef5c57a3208a8eb0c6d2
Matt Webb <matthew.webb@appfolio.com>
2019-08-21 16:37:59 -0700

    mw - add db image seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,26 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke' }, movie: movies.first)
+
+Image.create!([{ url: 'https://i.imgur.com/8c8mTIe.gif' },
+               { url: 'https://i.imgur.com/2JZKwrO.gif' },
+               { url: 'https://i.imgur.com/fYPX79m.gif' },
+               { url: 'https://i.imgur.com/QUTw7vq.gif' },
+               { url: 'https://i.imgur.com/Z82m0Bx.gif' },
+               { url: 'https://i.imgur.com/DzfVJox.gif' },
+               { url: 'https://i.imgur.com/dYwNBl1.gif' },
+               { url: 'https://i.imgur.com/MoEWqyg.gif' },
+               { url: 'https://i.imgur.com/Y8PIcM4.gif' },
+               { url: 'https://i.imgur.com/bVH1NBs.gif' },
+               { url: 'https://i.imgur.com/6xYdAcL.gif' },
+               { url: 'https://i.imgur.com/L08rEs5.gif' },
+               { url: 'https://i.imgur.com/DjVC927.gif' },
+               { url: 'https://i.imgur.com/vNjmuxJ.gif' },
+               { url: 'https://i.imgur.com/2JQMDAf.gif' },
+               { url: 'https://i.imgur.com/1P34SRJ.gif' },
+               { url: 'https://i.imgur.com/F5Ob14M.gif' },
+               { url: 'https://i.imgur.com/uDQsv4I.gif' },
+               { url: 'https://i.imgur.com/O9CjbYV.gif' },
+               { url: 'https://i.imgur.com/FjmdNUi.gif' },
+               { url: 'https://i.imgur.com/r3L0w9N.gif' }])
