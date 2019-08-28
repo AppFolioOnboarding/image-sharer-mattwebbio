9fba65ce3204e02f2051a2e4127c7c5b8ccc3a06
Matt Webb <matthew.webb@appfolio.com>
2019-08-28 11:23:44 -0700

    mw - Add React comment submission form

--- app/javascript/image_sharing/components/CommentForm.js
+++ app/javascript/image_sharing/components/CommentForm.js
@@ -1,18 +1,56 @@
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Alert } from 'reactstrap';
 import React from 'react';
+import { formEventToValues, post } from '../utils/helper';
 
-export default function CommentForm() {
-  return (
-    <Form>
-      <FormGroup>
-        <Label for="name">Your name:</Label>
-        <Input type="text" name="name" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="comment">Comments:</Label>
-        <Input type="textarea" name="comment" />
-      </FormGroup>
-      <Button color="primary">Submit</Button>
-    </Form>
-  );
+
+export default class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: {
+        text: '',
+        color: ''
+      }
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    return post('/api/feedbacks', formEventToValues(event)).catch(() => {
+      this.setState({
+        message: {
+          text: 'There was an error submitting your feedback :(',
+          color: 'danger'
+        }
+      });
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <FormGroup>
+            <Label for="name">Your name:</Label>
+            <Input type="text" name="name" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="comment">Comments:</Label>
+            <Input type="textarea" name="comment" />
+          </FormGroup>
+          <Button color="primary" type="submit">Submit</Button>
+        </Form>
+
+        {this.state.message.text &&
+          <div className="pt-5">
+            <Alert color={this.state.message.color}>
+              {this.state.message.text}
+            </Alert>
+          </div>
+        }
+      </div>
+    );
+  }
 }
--- app/javascript/image_sharing/test/components/CommentForm.spec.js
+++ app/javascript/image_sharing/test/components/CommentForm.spec.js
@@ -1,14 +1,74 @@
 /* eslint-env mocha */
 
-import { shallow } from 'enzyme/build';
-import assert from 'assert';
+import sinon from 'sinon';
+import { shallow, mount } from 'enzyme/build';
+import { expect, assert } from 'chai';
 import React from 'react';
 import CommentForm from '../../components/CommentForm';
+import * as Helper from '../../utils/helper';
 
 describe('<CommentForm />', () => {
   it('should render correctly', () => {
     const wrapper = shallow(<CommentForm />);
     assert(wrapper.contains('Your name:'));
     assert(wrapper.contains('Comments:'));
+  });
+
+  it('should show alert when message state is set', () => {
+    const wrapper = shallow(<CommentForm />);
+    wrapper.setState({
+      message: {
+        text: 'There was an error submitting your feedback :(',
+        color: 'danger'
+      }
+    });
+
+    assert(wrapper.contains('There was an error submitting your feedback :('));
+  });
+
+  describe('onSubmit', () => {
+    let sandbox;
+    let formSubmit;
+
+    beforeEach(() => {
+      sandbox = sinon.createSandbox();
+      formSubmit = {
+        target: { elements: [] },
+        preventDefault: () => {}
+      };
+    });
+
+    afterEach(() => {
+      sandbox.restore();
+    });
+
+    it('sets results when API request succeeds', () => {
+      const wrapper = mount(<CommentForm />);
+      const stub = sandbox.stub(Helper, 'post').returns(Promise.resolve());
+      sandbox.stub(Helper, 'formEventToValues').returns('formValues');
+
+      return wrapper.instance().handleSubmit(formSubmit).then(() => {
+        expect(stub.getCall(0).args[0]).to.equal('/api/feedbacks');
+        expect(stub.getCall(0).args[1]).to.equal('formValues');
+
+        assert.isEmpty(wrapper.state().message.text);
+        assert.isEmpty(wrapper.state().message.color);
+      });
+    });
+
+    it('sets error when API request fails', () => {
+      const wrapper = mount(<CommentForm />);
+      const stub = sandbox.stub(Helper, 'post').returns(Promise.reject());
+      sandbox.stub(Helper, 'formEventToValues').returns('formValues');
+
+      return wrapper.instance().handleSubmit(formSubmit).then(() => {
+        expect(stub.getCall(0).args[0]).to.equal('/api/feedbacks');
+        expect(stub.getCall(0).args[1]).to.equal('formValues');
+
+        expect(wrapper.state().message.text)
+          .to.equal('There was an error submitting your feedback :(');
+        expect(wrapper.state().message.color).to.equal('danger');
+      });
+    });
   });
 });
